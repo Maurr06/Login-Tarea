b2f3d4860cd3cda70f90e2462d0a3b029fd9e91b
MauroGITGlobal <maurrx1@gmail.com>
2024-11-29 13:45:32 -0300

    correccion segun devolucion

--- loginTask/urls.py
+++ loginTask/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),
-    path('', include('tasks.urls')),
     path('accounts/', include('accounts.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/')),
     
 ]
